86dd228ed3a7e1e6a8caaf22398b98f2455ddcf4
anthonyjdam <anthonydam1968@gmail.com>
2023-11-11 16:06:28 -0700

    testing api post from frontend

--- client/front-end/src/App.jsx
+++ client/front-end/src/App.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+
+
+  useEffect(() => {
+    
+    fetch("http://127.0.0.1:8000/createUser", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "Hilton",
+        age: 10
+      })
+    }).then( (res) => {
+      console.log(res); 
+    })
+  }, [])
+
+
+  return (
+  <div>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  </div>)
 }
