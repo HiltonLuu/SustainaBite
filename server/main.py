c50b6fcb60daece71a9905e61a0703d0566160a3
HiltonLuu <60626050+HiltonLuu@users.noreply.github.com>
2023-11-11 16:09:38 -0700

    trying to set up db

--- server/main.py
+++ server/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from database import init_db, SessionLocal
+from dependencies import get_db
+from sqlalchemy.orm import Session
+from models import User
+
 
 app = FastAPI()
+init_db()
 
 @app.get("/")
 def read_root():
@@ -9,3 +15,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, query_param: str = None):
     return {"item_id": item_id, "query_param": query_param}
+
+@app.post("/users/")
+def create_user(username: str, email: str, db: Session = Depends(get_db)):
+    # Use the db session to interact with the database
+    db_user = User(username=username, email=email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
