495be655f5bbcc81a9b9c3068a07b60c461186ce
BonnieWohoo <bonniewfwu@gmail.com>
2023-11-12 02:36:44 -0700

    Update BusinessLanding.jsx
    Made it work with the data

--- client/front-end/src/pages/BusinessLanding.jsx
+++ client/front-end/src/pages/BusinessLanding.jsx
@@ -17,6 +17,7 @@ export default function BusinessLanding() {
         "https://images.pexels.com/photos/4393021/pexels-photo-4393021.jpeg",
       company_img:
         "https://images.pexels.com/photos/1162361/pexels-photo-1162361.jpeg",
+      npo_name: "The Mustard Seed"
     },
     {
       name: "Cool Hotel",
@@ -30,6 +31,7 @@ export default function BusinessLanding() {
         "https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg",
       company_img:
         "https://images.pexels.com/photos/1162361/pexels-photo-1162361.jpeg",
+      npo_name: "The Mustard Seed"
     },
     {
       name: "Cool Hotel",
@@ -43,6 +45,7 @@ export default function BusinessLanding() {
         "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?",
       company_img:
         "https://images.pexels.com/photos/1162361/pexels-photo-1162361.jpeg",
+      npo_name: "The Mustard Seed"
     },
     {
       name: "Cool Hotel",
@@ -56,79 +59,82 @@ export default function BusinessLanding() {
         "https://images.pexels.com/photos/2664216/pexels-photo-2664216.jpeg",
       company_img:
         "https://images.pexels.com/photos/1162361/pexels-photo-1162361.jpeg",
+      npo_name: "The Mustard Seed"
     },
   ];
 
   /*put the data array here for the items in the drawer*/
 
   return (
-
     <div>
-          {data.map((item, index) => (
-      <div key={index}>
       <div className="collapse bg-base-200 mr-10 ml-10 mt-20 w-auto">
+
         <input type="checkbox" />
         <div className="collapse-title text-xl font-medium">
           Click to see your confirmed orders
         </div>
+
         <div className="collapse-content">
           <div className="carousel gap-4">
-            <div className="card card-compact w-48 bg-base-100 carousel-item p-0 w-48 h-72">
-              <figure className="h-48">
-                <img src={contactImage} alt="Food" />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">Food!
-                  <div className="badge badge-accent badge-sm">$5.00</div>
-                  <div className="badge badge-ghost badge-sm line-through">
-                    $10.00
-                  </div>
-                </h2>
-                <p className="text-sm text-left">
-                  <span className="font-bold text-sm">Pick up by:</span> NPO
-                </p>
-                <div class="h-16 overflow-hidden">
-                  <p class="overflow-ellipsis text-sm text-left">
-                    Your long paragraph text goes here. If it is too long, it will be truncated with an ellipsis.
-                  </p>
-                </div>
-                <div className="card-actions justify-end">
-                <label htmlFor="my_modal_6" className="btn btn-primary btn-sm">Check Info</label>
-                <input type="checkbox" id="my_modal_6" className="modal-toggle" />
-                <div className="modal modal-middle">
-                  <div className="modal-box justify-center pr-0 pl-0 pt-0 max-w-xl">
-                    <figure className="h-48 w-full"><img className="rounded object-contain" src={contactImage} /></figure>
-                    <div className="flex m-10 no-flex">
-                      <div className="w-72 pr-8">
-                        <h1 className="text-3xl font-bold text-left">Ramen</h1>
-                        <p className="text-left text-black pt-2 pb-2">
-                          <span className="font-bold">Pick up by:</span> NPO
-                        </p>
-                        <p className="text-left text-black pt-2 pb-2">
-                          <span className="font-bold">Pick up time:</span> 6:00PM
-                        </p>
-                        <p className="text-left text-black pt-2 pb-2">
-                          <span className="font-bold">Price:</span> $5.00
-                        </p>
-                        <p className="text-left text-black pt-2 pb-2">
-                          <span className="font-bold">Retail Price:</span> $10.00
-                        </p>
-                      </div>
-                      <div className="text-left">
-                        <p class="font-bold">Description:</p>
-                        <p>This is the paragraph text that follows the bolded</p>
+            {data.map((item, index) => (
+              <div key={index}>
+                <div className="card card-compact w-48 bg-base-100 carousel-item p-0 w-48 h-72">
+                  <figure className="h-48">
+                    <img src={item.food_img} alt="Food" />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title text-sm">{item.type}
+                      <div className="badge badge-accent badge-sm">{item.price}</div>
+                      <div className="badge badge-ghost badge-sm line-through">{item.retail_price}</div>
+                    </h2>
+                    <p className="text-sm text-left">
+                      <span className="font-bold text-sm">Pick up by:</span>{item.npo_name}
+                    </p>
+                    <div class="h-16 overflow-hidden">
+                      <p class="overflow-ellipsis text-sm text-left">
+                        {item.desc}
+                      </p>
+                    </div>
+                    <div className="card-actions justify-end">
+                      <label htmlFor="my_modal_6" className="btn btn-primary btn-sm">Check Info</label>
+                      <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+                      <div className="modal modal-middle">
+                        <div className="modal-box justify-center pr-0 pl-0 pt-0 max-w-xl">
+                          <figure className="h-48 w-full"><img className="rounded object-contain" src={item.food_img} /></figure>
+                          <div className="flex m-10 no-flex">
+                            <div className="w-72 pr-8">
+                              <h1 className="text-3xl font-bold text-left">{item.type}</h1>
+                              <p className="text-left text-black pt-2 pb-2">
+                                <span className="font-bold">Pick up by:</span>{item.npo_name}
+                              </p>
+                              <p className="text-left text-black pt-2 pb-2">
+                                <span className="font-bold">Pick up before:</span> {item.pickup}
+                              </p>
+                              <p className="text-left text-black pt-2 pb-2">
+                                <span className="font-bold">Price: $</span> {item.price}
+                              </p>
+                              <p className="text-left text-black pt-2 pb-2">
+                                <span className="font-bold">Retail Price: $</span> {item.retail_price}
+                              </p>
+                            </div>
+                            <div className="text-left">
+                              <p class="font-bold">Description:</p>
+                              <p>{item.desc}</p>
+                            </div>
+                          </div>
+                          <div className="modal-action">
+                            <label htmlFor="my_modal_6" className="btn mr-4">Close!</label>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <div className="modal-action">
-                      <label htmlFor="my_modal_6" className="btn mr-4">Close!</label>
-                    </div>
                   </div>
-                </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
+
       </div>
       <div className="flex justify-center p-4">
         <div className="grid grid-cols-4 gap-4 m-20">
@@ -151,29 +157,29 @@ export default function BusinessLanding() {
                         <label className="label">
                           <span className="label-text">Enter your listing name</span>
                         </label>
-                        <input type="text" placeholder="Type here" className="input input-bordered"/>
+                        <input type="text" placeholder="Type here" className="input input-bordered" />
                       </div>
                       <div className="form-control">
                         <label className="label">
                           <span className="label-text">Enter price</span>
                         </label>
-                        <input type="number" placeholder="$0.00" className="input input-bordered"/>
+                        <input type="number" placeholder="$0.00" className="input input-bordered" />
                       </div>
                       <div className="form-control">
                         <label className="label">
                           <span className="label-text">Enter retail price</span>
                         </label>
-                        <input type="number" placeholder="$0.00" className="input input-bordered"/>
+                        <input type="number" placeholder="$0.00" className="input input-bordered" />
                       </div>
                       <div className="form-control">
                         <label className="label">
-                          <span className="label-text">Enter pickup date</span>
+                          <span className="label-text">Enter pickup before date</span>
                         </label>
-                        <input type="date" placeholder="$0.00" className="input input-bordered"/>
+                        <input type="date" placeholder="$0.00" className="input input-bordered" />
                       </div>
                       <div className="form-control">
                         <label className="label">
-                          <span className="label-text">Enter pickup time</span>
+                          <span className="label-text">Enter pickup before time </span>
                         </label>
                         <input type="time" placeholder="$0.00" className="input input-bordered" />
                       </div>
@@ -196,8 +202,7 @@ export default function BusinessLanding() {
             </div>
           </div>
 
-          
-           {data.map((item, index) => (
+          {data.map((item, index) => (
             <div key={index}>
               <div className="card card-compact w-auto bg-base-100 shadow-xl p-0 ">
                 <figure className="h-48 w-auto">
@@ -218,37 +223,37 @@ export default function BusinessLanding() {
                   <p className="text-left">{item.desc}</p>
                   <p className="text-left">Pickup by: {item.pickup}</p>
                   <div className="card-actions justify-end">
-                  <label htmlFor="my_modal_7" className="btn btn-primary btn-sm">Check Info</label>
-                <input type="checkbox" id="my_modal_7" className="modal-toggle" />
-                <div className="modal modal-middle">
-                  <div className="modal-box justify-center pr-0 pl-0 pt-0 max-w-xl">
-                    <figure className="h-48 w-full"><img className="rounded object-contain" src={item.food_img}/></figure>
-                    <div className="flex m-10 no-flex">
-                      <div className="w-72 pr-8">
-                        <h1 className="text-3xl font-bold text-left">{item.type}</h1>
-                        <p className="text-left text-black pt-2 pb-2">
-                          <span className="font-bold">Pick up by: </span>{item.npo_name}
-                        </p>
-                        <p className="text-left text-black pt-2 pb-2">
-                          <span className="font-bold">Pick up time: </span>{item.pickup}
-                        </p>
-                        <p className="text-left text-black pt-2 pb-2">
-                          <span className="font-bold">Price: </span>{item.price}
-                        </p>
-                        <p className="text-left text-black pt-2 pb-2">
-                          <span className="font-bold">Retail Price: </span>{item.retail_price}
-                        </p>
-                      </div>
-                      <div className="text-left">
-                        <p class="font-bold">Description:</p>
-                        <p>{item.desc}</p>
+                    <label htmlFor="my_modal_7" className="btn btn-primary btn-sm">Check Info</label>
+                    <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+                    <div className="modal modal-middle">
+                      <div className="modal-box justify-center pr-0 pl-0 pt-0 max-w-xl">
+                        <figure className="h-48 w-full"><img className="rounded object-contain" src={item.food_img} /></figure>
+                        <div className="flex m-10 no-flex">
+                          <div className="w-72 pr-8">
+                            <h1 className="text-3xl font-bold text-left">{item.type}</h1>
+                            <p className="text-left text-black pt-2 pb-2">
+                              <span className="font-bold">Pick up by: </span>{item.npo_name}
+                            </p>
+                            <p className="text-left text-black pt-2 pb-2">
+                              <span className="font-bold">Pick up time: </span>{item.pickup}
+                            </p>
+                            <p className="text-left text-black pt-2 pb-2">
+                              <span className="font-bold">Price: </span>{item.price}
+                            </p>
+                            <p className="text-left text-black pt-2 pb-2">
+                              <span className="font-bold">Retail Price: </span>{item.retail_price}
+                            </p>
+                          </div>
+                          <div className="text-left">
+                            <p class="font-bold">Description:</p>
+                            <p>{item.desc}</p>
+                          </div>
+                        </div>
+                        <div className="modal-action">
+                          <label htmlFor="my_modal_7" className="btn mr-4">Close!</label>
+                        </div>
                       </div>
                     </div>
-                    <div className="modal-action">
-                      <label htmlFor="my_modal_7" className="btn mr-4">Close!</label>
-                    </div>
-                  </div>
-                </div>
                   </div>
                 </div>
               </div>
