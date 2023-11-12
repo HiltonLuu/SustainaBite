1bdbc9b0ddf371495bb3ee642b0351a35090808b
anthonyjdam <anthonydam1968@gmail.com>
2023-11-12 02:15:30 -0700

    finish payment flow

--- client/front-end/src/MainRouter.jsx
+++ client/front-end/src/MainRouter.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import BusinessLanding from "./pages/BusinessLanding";
 import NPOLanding from "./pages/NPOLanding";
 import Home from "./pages/Home";
+import NPORequest from "./pages/NPORequest";
+import NPOOrder from "./pages/NPOOrder";
+import NPOPay from "./pages/NPOPay"
 
 const MainRouter = () => {
   return (
@@ -10,6 +13,9 @@ const MainRouter = () => {
       <Route path="/" element={<Home />}></Route>
       <Route path="/business" element={<BusinessLanding />}></Route>
       <Route path="/not-for-profit" element={<NPOLanding />}></Route>
+      <Route path="/not-for-profit/request/:id" element={<NPORequest />}></Route>
+      <Route path="/not-for-profit/order/:id" element={<NPOOrder />}></Route>
+      <Route path="/not-for-profit/pay/:id" element={<NPOPay />}></Route>
     </Routes>
   );
 };
