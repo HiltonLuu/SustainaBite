436ca5ef25f6521541205f713daa3f97d6f10ebc
BonnieWohoo <bonniewfwu@gmail.com>
2023-11-11 19:53:17 -0700

    implement theme

--- client/front-end/src/pages/BusinessLanding.jsx
+++ client/front-end/src/pages/BusinessLanding.jsx
@@ -11,9 +11,9 @@ export default function BusinessLanding() {
         <div className="collapse-title text-xl font-medium">
           Open to see your confirmed orders
         </div>
-        <div className="collapse-content align-top">
+        <div className="collapse-content">
           <div className="carousel gap-4">
-            <div className="card card-compact w-auto bg-base-100 carousel-item p-0 w-48 h-72">
+            <div className="card card-compact w-48 bg-base-100 carousel-item p-0 w-48 h-72">
               <figure><img src={contactImage} alt="Food" /></figure>
               <div className="card-body">
                 <h2 className="card-title">Food!</h2>
@@ -23,7 +23,7 @@ export default function BusinessLanding() {
                 </div>
               </div>
             </div>
-            <div className="card card-compact w-auto bg-base-100 carousel-item p-0 w-48 h-72">
+            <div className="card card-compact w-48 bg-base-100 carousel-item p-0 w-48 h-72">
               <figure><img src={contactImage} alt="Food" /></figure>
               <div className="card-body">
                 <h2 className="card-title">Food!</h2>
@@ -33,7 +33,7 @@ export default function BusinessLanding() {
                 </div>
               </div>
             </div>
-            <div className="card card-compact w-auto bg-base-100 carousel-item p-0 w-48 h-72">
+            <div className="card card-compact w-48 bg-base-100 carousel-item p-0 w-48 h-72">
               <figure><img src={contactImage} alt="Food" /></figure>
               <div className="card-body">
                 <h2 className="card-title">Food!</h2>
@@ -48,13 +48,31 @@ export default function BusinessLanding() {
       </div>
       <div className="flex justify-center p-4">
         <div className="grid grid-cols-4 gap-4">
-          <div className="card card-compact w-auto bg-base-100 shadow-xl p-0">
+                          <div className="card card-compact w-auto bg-base-100 shadow-xl p-0 justify-center">
+                          {/* Open the modal using document.getElementById('ID').showModal() method */}
+                          <button className="btn btn-primary" onClick={() => document.getElementById('my_modal_1').showModal()}>Reserve Now</button>
+                <dialog id="my_modal_1" className="modal">
+                  <div className="modal-box justify-center">
+                    <h3 className="font-bold text-lg">Create a new listing!</h3>
+                    <input type="file" className="file-input file-input-bordered w-full max-w-xs" />
+                    <div className="form-control w-full max-w-xs">
+                      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="modal-action">
+                      <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn">Close</button>
+                      </form>
+                    </div>
+                  </div>
+                </dialog>
+                </div>
+          <div className="card card-compact w-auto bg-base-100 shadow-xl p-0 ">
             <figure className="h-48 w-auto"><img className="object-contain" src="https://images.pexels.com/photos/17593640/pexels-photo-17593640/free-photo-of-a-bowl-of-ramen-with-chopsticks-and-eggs.jpeg" alt="Food" /></figure>
             <div className="card-body">
               <h2 className="card-title">Food!</h2>
               <p>This is a food card!</p>
               <div className="card-actions justify-end">
-                <button className="btn btn-primary">Reserve Now</button>
               </div>
             </div>
           </div>
