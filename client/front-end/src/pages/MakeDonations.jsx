af9ee35841b548708059b4294aa84278fba5390f
BonnieWohoo <bonniewfwu@gmail.com>
2023-11-12 03:09:26 -0700

    Update MakeDonations.jsx
    Implemented Modals

--- client/front-end/src/pages/MakeDonations.jsx
+++ client/front-end/src/pages/MakeDonations.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "../App.css";
 
 export default function MakeDonations() {
-  
+
     const data = [
         {
             name: "The Mustard Seed",
@@ -50,24 +50,62 @@ export default function MakeDonations() {
             <div className="grid grid-cols-3 gap-10">
                 {data.map((company, index) => (
                     <div key={index} className="card card-compact w-96 bg-base-100 shadow-xl p-0">
-                    <figure className="h-48 w-auto">
-                        <img className="object-contain"
-                        src={company.company_img}
-                        alt="Company Logo" />
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title text-black">{company.name}</h2>
-                        <p className="text-left">{company.about}</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-secondary">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                                </svg>
-                                Donate
-                            </button>
+                        <figure className="h-48 w-auto">
+                            <img className="object-contain"
+                                src={company.company_img}
+                                alt="Company Logo" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title text-black">{company.name}</h2>
+                            <p className="text-left">{company.about}</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-secondary" onClick={() => document.getElementById('my_modal_3').showModal()}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                    </svg>
+                                    Donate
+                                </button>
+                                <dialog id="my_modal_3" className="modal">
+                                    <div className="modal-box max-w-2xl">
+                                        <form method="dialog">
+                                            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                                        </form>
+                                        <h1>Make a donation!</h1>
+                                        <div className="flex m-10 no-flex">
+                                            <div className="flex-1 align-top">
+                                                <figure className=""><img className="rounded object-contain" src={company.company_img} /></figure>
+                                                <p>{company.about}</p>
+                                            </div>
+                                            <div className="flex-1 align-top p-4 items-center">
+                                                <div className="flex gap-4">
+                                                    <div className="form-control w-full max-w-xs">
+                                                        <label className="label">
+                                                            <span className="label-text">First Name</span>
+                                                        </label>
+                                                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                                                    </div>
+                                                    <div className="form-control w-full max-w-xs">
+                                                        <label className="label">
+                                                            <span className="label-text">Last Name</span>
+                                                        </label>
+                                                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                                                    </div>
+                                                </div>
+                                                <div className="form-control w-full max-w-xs pb-4">
+                                                        <label className="label">
+                                                            <span className="label-text">Amount to donate: </span>
+                                                        </label>
+                                                        <input type="number" placeholder="$0.00" className="input input-bordered w-full max-w-xs" />
+                                                    </div>
+                                                    <button className="btn"> Confirm </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </dialog>
+
+                            </div>
                         </div>
                     </div>
-                </div>
                 ))}
             </div>
 
