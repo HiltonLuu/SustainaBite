48d3834071dd3d494b3e2dbfa8a27e5b5c980e4b
BonnieWohoo <bonniewfwu@gmail.com>
2023-11-12 01:28:16 -0700

    Merge pull request #17 from HiltonLuu/anthony-dev
    Implemented popup screen for confirmed orders

--- client/front-end/src/components/NavBar.jsx
+++ client/front-end/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   const { currentUser } = useContext(UserContext);
 
   return (
-    <div className="bg-gray-800 text-white p-4 fixed top-0 w-full">
+    <div className="bg-gray-800 text-white p-4 fixed top-0 w-full absolute">
       <div className="flex items-center justify-between w-full">
         <p href="#" className="text-xl font-bold">
           Your Logo
--- client/front-end/src/pages/BusinessLanding.jsx
+++ client/front-end/src/pages/BusinessLanding.jsx
@@ -63,46 +63,64 @@ export default function BusinessLanding() {
 
   return (
     <div>
-      <div className="collapse bg-base-200">
+      <div className="collapse bg-base-200 mr-10 ml-10 mt-20 w-auto">
         <input type="checkbox" />
         <div className="collapse-title text-xl font-medium">
-          Open to see your confirmed orders
+          Click to see your confirmed orders
         </div>
         <div className="collapse-content">
           <div className="carousel gap-4">
             <div className="card card-compact w-48 bg-base-100 carousel-item p-0 w-48 h-72">
-              <figure>
+              <figure className="h-48">
                 <img src={contactImage} alt="Food" />
               </figure>
               <div className="card-body">
-                <h2 className="card-title">Food!</h2>
-                <p>This is a food card!</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Check Order</button>
+                <h2 className="card-title">Food!
+                  <div className="badge badge-accent badge-sm">$5.00</div>
+                  <div className="badge badge-ghost badge-sm line-through">
+                    $10.00
+                  </div>
+                </h2>
+                <p className="text-sm text-left">
+                  <span className="font-bold text-sm">Pick up by:</span> NPO
+                </p>
+                <div class="h-16 overflow-hidden">
+                  <p class="overflow-ellipsis text-sm text-left">
+                    Your long paragraph text goes here. If it is too long, it will be truncated with an ellipsis.
+                  </p>
                 </div>
-              </div>
-            </div>
-            <div className="card card-compact w-48 bg-base-100 carousel-item p-0 w-48 h-72">
-              <figure>
-                <img src={contactImage} alt="Food" />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">Food!</h2>
-                <p>This is a food card!</p>
                 <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Check Order</button>
+                <label htmlFor="my_modal_6" className="btn btn-primary btn-sm">Check Info</label>
+                <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+                <div className="modal modal-middle">
+                  <div className="modal-box justify-center pr-0 pl-0 pt-0">
+                    <figure className="h-48 w-full"><img className="rounded object-contain" src={contactImage} /></figure>
+                    <div className="flex m-10 no-flex">
+                      <div className="w-72 pr-8">
+                        <h1 className="text-3xl font-bold text-left">Ramen</h1>
+                        <p className="text-left text-black pt-2 pb-2">
+                          <span className="font-bold">Pick up by:</span> NPO
+                        </p>
+                        <p className="text-left text-black pt-2 pb-2">
+                          <span className="font-bold">Pick up time:</span> 6:00PM
+                        </p>
+                        <p className="text-left text-black pt-2 pb-2">
+                          <span className="font-bold">Price:</span> $5.00
+                        </p>
+                        <p className="text-left text-black pt-2 pb-2">
+                          <span className="font-bold">Retail Price:</span> $10.00
+                        </p>
+                      </div>
+                      <div className="text-left">
+                        <p class="font-bold">Description:</p>
+                        <p>This is the paragraph text that follows the bolded</p>
+                      </div>
+                    </div>
+                    <div className="modal-action">
+                      <label htmlFor="my_modal_6" className="btn mr-4">Close!</label>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="card card-compact w-48 bg-base-100 carousel-item p-0 w-48 h-72">
-              <figure>
-                <img src={contactImage} alt="Food" />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">Food!</h2>
-                <p>This is a food card!</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Check Order</button>
                 </div>
               </div>
             </div>
@@ -110,19 +128,19 @@ export default function BusinessLanding() {
         </div>
       </div>
       <div className="flex justify-center p-4">
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-4 gap-4 m-20">
           <div className="card card-compact w-auto bg-base-100 shadow-xl p-0 justify-center">
             <div className="flex justify-center">
               {/* Open the modal using document.getElementById('ID').showModal() method */}
               <button
                 className="btn btn-primary w-48"
                 onClick={() =>
-                  document.getElementById("my_modal_1").showModal()
+                  document.getElementById("my_modal_2").showModal()
                 }
               >
                 Create Listing
               </button>
-              <dialog id="my_modal_1" className="modal">
+              <dialog id="my_modal_2" className="modal">
                 <div className="modal-box justify-center">
                   <h3 className="font-bold text-lg">Create a new listing!</h3>
                   <input
