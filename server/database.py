b2a63b7abbb62b2dcba7f75f78abb1d72c69ef06
anthonyjdam <anthonydam1968@gmail.com>
2023-11-11 17:55:58 -0700

    template db code

--- server/database.py
+++ server/database.py
@@ -1,13 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from fastapi import Depends
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
-def init_db():
-    Base.metadata.create_all(bind=engine)
