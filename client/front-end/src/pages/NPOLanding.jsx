b30c255dcf9d8bb331bf62e474043bad56d8335c
J-essicaTran <jessica.tran3@ucalgary.ca>
2023-11-12 15:28:24 +0000

    Minor styling changes to NPOLanding.jsx

--- client/front-end/src/pages/NPOLanding.jsx
+++ client/front-end/src/pages/NPOLanding.jsx
@@ -4,59 +4,59 @@ import "../App.css";
 export default function NPOLanding() {
   const data = [
     {
-      name: "Cool Hotel",
-      location: "123 Real Address",
-      type: "Cooked Yams (5lbs)",
-      pickup: "Nov 13th, 8:00pm",
-      price: 10,
-      retail_price: 20,
+      name: "Route 66 Hotel",
+      location: "5671 Poplar Drive",
+      type: "Vegatables (30bs)",
+      pickup: "Nov 14th, 6:30pm",
+      price: 20,
+      retail_price: 50,
       food_img:
-        "https://images.pexels.com/photos/4393021/pexels-photo-4393021.jpeg",
+        "https://images.pexels.com/photos/3988952/pexels-photo-3988952.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       company_img:
         "https://images.pexels.com/photos/1162361/pexels-photo-1162361.jpeg",
     },
     {
-      name: "Cool Hotel",
-      location: "123 Real Address",
-      type: "Cooked Yams (5lbs)",
-      pickup: "Nov 13th, 8:00pm",
+      name: "Local Street Cafe",
+      location: "960 Meadow Mist Lane",
+      type: "Baked Goods (15bs)",
+      pickup: "Nov 13th, 1:45pm",
       price: 10,
-      retail_price: 20,
+      retail_price: 30,
       food_img:
-        "https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg",
+        "https://images.pexels.com/photos/298217/pexels-photo-298217.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       company_img:
-        "https://images.pexels.com/photos/1162361/pexels-photo-1162361.jpeg",
+        "https://images.pexels.com/photos/2679323/pexels-photo-2679323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
     {
-      name: "Cool Hotel",
-      location: "123 Real Address",
-      type: "Cooked Yams (5lbs)",
-      pickup: "Nov 13th, 8:00pm",
-      price: 10,
-      retail_price: 20,
+      name: "Home Sweet Home Cafe",
+      location: "4658 Saffron Sunset Street",
+      type: "Baked Goods (12bs)",
+      pickup: "Nov 13th, 10:00am",
+      price: 8,
+      retail_price: 25,
       food_img:
-        "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?",
+        "https://images.pexels.com/photos/263070/pexels-photo-263070.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       company_img:
-        "https://images.pexels.com/photos/1162361/pexels-photo-1162361.jpeg",
+        "https://images.pexels.com/photos/4819658/pexels-photo-4819658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
     {
-      name: "Cool Hotel",
-      location: "123 Real Address",
-      type: "Cooked Yams (5lbs)",
-      pickup: "Nov 13th, 8:00pm",
-      price: 10,
-      retail_price: 20,
+      name: "Fish Creek Hotel",
+      location: "3701 Elm Avenue",
+      type: "Fruits (20bs)",
+      pickup: "Nov 15th, 8:00am",
+      price: 15,
+      retail_price: 30,
       food_img:
-        "https://images.pexels.com/photos/2664216/pexels-photo-2664216.jpeg",
+        "https://images.pexels.com/photos/1300975/pexels-photo-1300975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       company_img:
-        "https://images.pexels.com/photos/1162361/pexels-photo-1162361.jpeg",
+        "https://images.pexels.com/photos/1569002/pexels-photo-1569002.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
   ];
 
   return (
     <div className="mt-20">
 
-      <div className="pl-10 pt-20 pb-5">
+      <div className="pl-10 pt-20 pb-10">
         <h1 className="text-left">Order Near You...</h1>
         <div className="flex text-black">
           <svg
@@ -94,9 +94,9 @@ export default function NPOLanding() {
         </div>
       </div>
 
-      <div className="flex mt-10">
+      <div className="flex mt-10 mb-10">
 
-        <div className="text-left text-black w-96 pl-10">
+        <div className="text-left text-black w-1/4 pl-10 border-r-4">
             <p className="text-xl text-black">
                 <b>All Stores</b>
             </p>
@@ -105,7 +105,7 @@ export default function NPOLanding() {
             </p>
  
             <div class="flex mt-5">
-              <input type="checkbox" className="w-5 h-5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none bg-white border-gray-700 checked:bg-blue-500 checked:border-blue-500 focus:ring-offset-gray-800" id="hs-default-checkbox"/>
+            <input type="checkbox" className="w-5 h-5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" id="hs-default-checkbox"/>
               <label for="hs-default-checkbox" class="text-sm text-gray-500 ms-3 dark:text-black">Recommended</label>
             </div>
             <div class="flex mt-2">
@@ -123,7 +123,7 @@ export default function NPOLanding() {
 
         </div>
 
-        <div className="grid grid-cols-2 gap-10">
+        <div className="grid grid-cols-2 gap-16 mx-auto">
           {data.map((item, index) => (
             <div key={index} className="card w-96 bg-base-100 shadow-xl p-0">
               <figure className="h-48 w-auto">
