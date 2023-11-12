5094a05583a855b8fb836ab8614b6aa33c2db805
BonnieWohoo <bonniewfwu@gmail.com>
2023-11-11 18:32:24 -0700

    Update BusinessLanding.jsx
    Flex and gridbox

--- client/front-end/src/pages/BusinessLanding.jsx
+++ client/front-end/src/pages/BusinessLanding.jsx
@@ -6,19 +6,60 @@ export default function BusinessLanding() {
 
   return (
     <div>
-
-
-  <img src={contactImage} alt="Contact" />
-<div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src={contactImage} alt="Food" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Food!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-4 gap-4">
+        <div className="card w-auto bg-base-100 shadow-xl">
+          <figure><img src={contactImage} alt="Food" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Food!</h2>
+            <p>This is a food card!</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Reserve Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-auto bg-base-100 shadow-xl">
+          <figure><img src={contactImage} alt="Food" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Food!</h2>
+            <p>This is a food card!</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Reserve Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-auto bg-base-100 shadow-xl">
+          <figure><img src={contactImage} alt="Food" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Food!</h2>
+            <p>This is a food card!</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Reserve Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-auto bg-base-100 shadow-xl">
+          <figure><img src={contactImage} alt="Food" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Food!</h2>
+            <p>This is a food card!</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Reserve Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-auto bg-base-100 shadow-xl">
+          <figure><img src={contactImage} alt="Food" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Food!</h2>
+            <p>This is a food card!</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Reserve Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-</div>
+    </div>
   );
 }
