0078dbf547e2f0205613ca039768c75da1991b4a
HiltonLuu <60626050+HiltonLuu@users.noreply.github.com>
2023-11-12 01:27:42 -0700

    made homepage and sign-in

--- client/front-end/src/MainRouter.jsx
+++ client/front-end/src/MainRouter.jsx
@@ -3,13 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import BusinessLanding from "./pages/BusinessLanding";
 import NPOLanding from "./pages/NPOLanding";
 import Home from "./pages/Home";
+import DonationsPage from "./DonationsPage";
+import Signin from "./pages/Signin";
 
 const MainRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/sign-in" element={<Signin />}></Route>
       <Route path="/business" element={<BusinessLanding />}></Route>
       <Route path="/not-for-profit" element={<NPOLanding />}></Route>
+      <Route path="/donate" element={<DonationsPage />}></Route>
     </Routes>
   );
 };
